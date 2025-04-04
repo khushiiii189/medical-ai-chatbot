bde6e8a30e1cbffff06eeff8922a8d32109cf79a
Khushi Patel <109019717+khushiiii189@users.noreply.github.com>
2025-04-04 19:01:20 -0400

    Update Chatbot.js

--- frontend1/src/Chatbot.js
+++ frontend1/src/Chatbot.js
@@ -2,8 +2,7 @@ import React, { useState, useRef } from "react";
 import Recorder from "recorder-js";
 import "./Chatbot.css";
 
-const backendUrl = "https://chatbot-backend-p2a5.onrender.com";
-
+const backendUrl = "https://something-new-dtnj.onrender.com";
 const MedicalChatbot = () => {
   const [conversation, setConversation] = useState([]);
   const [diagnosis, setDiagnosis] = useState(null);
